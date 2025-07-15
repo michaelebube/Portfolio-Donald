1af8967b23afb41e493999812a799b73c5b5e115
Michael Ebube <michaelebube18@gmail.com>
2025-07-15 03:59:43 +0100

    Feat: Creat Contact Page and Add LinkedIn and GitHub icons to contact page and create SVG files for both

--- components/ProjCard.vue
+++ components/ProjCard.vue
@@ -3,7 +3,7 @@
        class="block relative border-b-2 border-t-2 border-l-2 border-[#222] border-r-0 rounded-[22px] p-[22px] pr-[30px] no-underline text-[#222] overflow-hidden transition-all duration-200 hover:scale-[1.02] focus:scale-[1.02] group" 
        tabindex="0">
       
-      <div class="border-b-[1.5px] border-[#222] mb-3 p-2 pr-0 pb-[8px] font-bold text-[1.1rem] leading-[1.3] sm:h-[105px] md:h-[80px]">
+      <div class="border-b-[1.5px] border-[#222] mb-3 p-2 pr-0 pb-[8px] font-bold text-[1.1rem] w-[95%] leading-[1.3] sm:h-[105px] md:h-[80px]">
         <h1>{{ title }}</h1>
       </div>
       
--- pages/contact.vue
+++ pages/contact.vue
@@ -1,1 +1,21 @@
-<template></template>
+<template>
+    <div class="grid grid-cols-[300px_1fr] ml-4 ">
+        <div class="flex flex-col">
+            <div class="flex h-full items-center min-h-[60vh] relative">
+                <div class="w-[300px]">
+                    <h1 class="border-b-[1px] w-[85%] text-6xl pb-4 leading-16 font-semibold">Let's get started</h1>
+                    <div class="flex gap-1 mt-4">
+                        <a href="">
+                            <img src="../assets/icons/linkedin_icon.svg" alt="LinkedIn Icon" class="w-10 h-10 hover:scale-110 transition-transform duration-200">
+                        </a>
+                        <a href="">
+                            <img src="../assets/icons/github_icon.svg" alt="GitHub Icon" class="w-10 h-10 hover:scale-110 transition-transform duration-200">
+                        </a>
+                    </div>
+                </div>
+                
+                <div class="absolute right-0 inset-y-[5%] border-r-[1px] border-gray-800 h-115"></div>
+            </div>
+        </div>
+    </div>
+</template>
