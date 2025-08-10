93ed650f39fe79715b99bd00bc53cee462c61f6e
Michael Ebube <michaelebube18@gmail.com>
2025-08-10 23:11:53 +0100

    fix: responsiveness issues on ipads were worked on

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,50 +1,48 @@
 <template>
-    <div class="grid grid-cols-[300px_1fr] ml-4 ">
+    <div class="grid lg:grid-cols-[300px_1fr]  md:grid-cols-[200px_1fr] lg:mx-4   "> 
         <div class="flex flex-col">
             <div class="flex h-full items-center min-h-[60vh] relative">
-                <div class="w-[300px]">
-                    <h1 class="border-b-[1px] w-[85%] lg:text-5xl xl:text-6xl pb-4 leading-16 font-semibold -mt-16">Let's get started</h1>
+                <div class="w-full max-w-[300px]">
+                    <h1 class="border-b-[1px] w-[85%] md:text-3xl lg:text-5xl xl:text-6xl lg:pb-4 md:pb-2 md:leading-12 lg:leading-16 font-semibold lg:-mt-30 md:-mt-5">Let's get started</h1>
                     <div class="flex gap-1 mt-4">
                         <a href="">
-                            <img src="../assets/icons/linkedin_icon.svg" alt="LinkedIn Icon" class="w-10 h-10 hover:scale-110 transition-transform duration-200">
+                            <img src="../assets/icons/linkedin_icon.svg" alt="LinkedIn Icon" class="md:w-8 md:h-8 lg:w-10 lg:h-10 hover:scale-110 transition-transform duration-200">
                         </a>
                         <a href="">
-                            <img src="../assets/icons/github_icon.svg" alt="GitHub Icon" class="w-10 h-10 hover:scale-110 transition-transform duration-200">
+                            <img src="../assets/icons/github_icon.svg" alt="GitHub Icon" class="md:w-8 md:h-8 lg:w-10 lg:h-10 hover:scale-110 transition-transform duration-200">
                         </a>
                     </div>
                 </div>
                 
-                <div class="absolute right-0 inset-y-[5%] border-r-[1px] border-gray-800 h-115"></div>
+                <div class="absolute right-0  xl:inset-y-[5%] lg:inset-y-[-4%] md:inset-y-[18px] limitedHeight:inset-y-[13.5%] border-r-[1px] border-gray-800 h-115"></div>
             </div>
         </div>
 
-        <div class="px-12 py-10">
-            <h1 class="text-xl xl:text-2xl text-[#1B263B] border-b-1 pb-4 border-b-[#1B263B]/20">You can reach me by filling the form below</h1>
+        <div class="md:px-8 lg:px-12 overflow-hidden md:-mt-3 py-10  lg:-mt-10 xl:mt-0 limitedHeight:mt-[20%] ">
+            <h1 class="text-lg lg:text-xl xl:text-2xl text-[#1B263B] border-b-1 pb-4 border-b-[#1B263B]/20">You can reach me by filling the form below</h1>
             <div class=" mt-6  rounded-lg font-sans">
-    <div class="flex flex-wrap items-baseline gap-2 leading-relaxed border-b-1 border-b-[#1B263B]/20 pb-8">
-      <span class="text-2xl xl:text-4xl font-semibold text-slate-800 mr-4">My name is</span>
+    <div class="flex flex-wrap items-baseline md:gap-2 lg:gap-2 leading-relaxed border-b-1 border-b-[#1B263B]/20 pb-8">
+      <span class="text-lg lg:text-2xl xl:text-4xl font-semibold text-slate-800 mr-4">My name is</span>
       <input 
         v-model="formData.fullName"
         type="text" 
         placeholder="YOUR FULL NAME"
-        class="bg-transparent border-0 border-b-2 border-black/40 text-black text-lg xl:text-3xl font-medium px-0 py-2 outline-none placeholder:text-xl  tracking-wide placeholder-[#9DA0A6]  focus:border-black max-w-96 flex-1 mr-4"
+        class="bg-transparent border-0 border-b-2 border-black/40 text-black  md:text-lg text-3xl font-medium px-0 py-2 outline-none placeholder:xl:text-xl placeholder:lg:text-lg placeholder:md:text-sm tracking-wide placeholder-[#9DA0A6]  focus:border-black max-w-100 flex-1 mr-4"
       />
-      <span class="text-2xl xl:text-4xl font-semibold text-slate-800">and I</span>
+      <span class="text-xl lg:text-2xl  xl:text-4xl font-semibold text-slate-800">and I</span>
       
-      <div class="xl:w-full xl:h-0"></div>
+      <span class="xl:w-full xl:h-0"></span>
       
-      <span class="text-2xl xl:text-4xl font-semibold text-slate-800 mr-4">have a</span>
+      <span class="text-xl lg:text-2xl  xl:text-4xl font-semibold text-slate-800 mr-4">have a</span>
       <input 
         v-model="formData.jobType"
         type="text" 
         placeholder="ONE-TIME GIG, PART-TIME JOB, FULL-TIME JOB ETC"
-        class="bg-transparent border-0 border-b-2 border-black/40 text-black font-medium text-lg xl:text-3xl  px-0 py-2 outline-none placeholder:text-xl  tracking-wide placeholder-[#9DA0A6] focus:border-black min-w-96 flex-1 mr-4"
+        class="bg-transparent border-0 border-b-2 border-black/40 text-black font-medium text-lg xl:text-3xl  px-0 py-2 placeholder:md:text-sm placeholder:lg:text-lg  outline-none placeholder:xl:text-xl  tracking-wide placeholder-[#9DA0A6] focus:border-black min-w-100  flex-1 mr-4"
       />
-      <span class="text-2xl xl:text-4xl font-semibold text-slate-800">that</span>
+      <span class="text-xl lg:text-2xl  xl:text-4xl font-semibold text-slate-800">that</span>
       
-      <div class="xl:w-full xl:h-0"></div>
-      
-      <span class="text-2xl xl:text-4xl font-semibold text-slate-800">needs help. You can reach me at</span>
+      <!-- <div class="xl:w-full xl:h-0"></div> --><span class="text-xl lg:text-2xl   xl:text-4xl font-semibold text-slate-800">needs help. You can reach me at</span>
       
       <div class="xl:w-full xl:h-0"></div>
       
@@ -52,13 +50,13 @@
         v-model="formData.email"
         type="email" 
         placeholder="YOUR EMAIL ADDRESS"
-        class="bg-transparent border-0 border-b-2 border-black/40 text-black font-medium text-lg xl:text-3xl  px-0 py-2 outline-none placeholder:text-xl tracking-wide placeholder-[#9DA0A6]  focus:border-black max-w-130 flex-1 mr-4"
+        class="bg-transparent border-0 border-b-2 border-black/40 text-black font-medium text-lg xl:text-3xl  px-0 py-2 outline-none placeholder:md:text-sm placeholder:lg:text-lg placeholder:xl:text-xl tracking-wide placeholder-[#9DA0A6]  focus:border-black max-w-130 flex-1 mr-4"
       />
-      <span class="text-2xl xl:text-4xl font-semibold text-slate-800">to get</span>
+      <span class="text-xl lg:text-2xl   xl:text-4xl font-semibold text-slate-800">to get</span>
       
       <div class="w-full h-0"></div>
       
-      <span class="text-2xl xl:text-4xl font-semibold text-slate-800">things started.</span>
+      <span class="text-xl lg:text-2xl  xl:text-4xl font-semibold text-slate-800">things started.</span>
     </div>
   </div>
   <div class="relative mt-6">
