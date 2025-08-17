e3ee448d3029facec3618c87c5f5253d3762bdbb
Michael Ebube <michaelebube18@gmail.com>
2025-08-17 02:53:20 +0100

    Added vercel analytics

--- layouts/default.vue
+++ layouts/default.vue
@@ -23,10 +23,12 @@
         </div>
     </div>
     <SpeedInsights />
+    <Analytics />
 </template>
 
 <script setup lang="ts">
 import { SpeedInsights } from "@vercel/speed-insights/nuxt"
+import { Analytics } from '@vercel/analytics/nuxt'
 
 const route = useRoute()
 
